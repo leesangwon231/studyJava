9f5b4e9b44db740d8c09c550150c97a4ed7f6dd6
Sangwon Lee <werggf@naver.com>
2022-08-26 19:45:08 +0900

    자바의신 5장

---
+++ Doit5.java
@@ -0,0 +1,60 @@
+public class Doit5 {
+
+    /**
+     * 월급여에서 세금을 뺀 실급여 계산하는 함수
+     * @param yearSalary 연봉 입력
+     * @return 실급여 반환
+     */
+    public double getMonthlySalary(int yearSalary){
+        double monthlySalary = yearSalary / 12.0 ;
+        
+        double health =  calculateHealth(monthlySalary);
+        double income = calculateTax(monthlySalary);
+        double nation = calculateNation(monthlySalary);
+
+        monthlySalary -= health;
+        monthlySalary -= income;
+        monthlySalary -= nation;
+
+        return monthlySalary;
+    }
+
+    /**
+     * 근로소득세 계산
+     * @param monthlySalary 월급 입력
+     * @return 월급에 대한 근로소득세 반환
+     */
+    public double calculateTax(double monthlySalary){
+        double incomeTax = monthlySalary * 0.125;
+        System.out.println("근로소득 => " + incomeTax);
+        return  incomeTax;
+    }
+
+    /**
+     * 연금 계산
+     * @param monthlySalary 월급 입력
+     * @return 월급에 대한 연금 반환
+     */
+    public double calculateNation(double monthlySalary){
+        double nation = monthlySalary * 0.081;
+        System.out.println("연금 => " + nation);
+        return nation;
+    }
+
+    /**
+     * 건강 보험료
+     * @param monthlySalary 월급여 입력
+     * @return 월급여에 대한 건강 보험료 반환
+     */
+    public double calculateHealth(double monthlySalary){
+       double health = monthlySalary*0.135;
+       System.out.println("건강보험 => " + health);
+       return health; 
+    }
+
+
+    public static void main(String[] args) {
+        Doit5 sample = new Doit5();
+        System.out.println(sample.getMonthlySalary(20000000));
+    }
+}
---
+++ Operators5.java
@@ -0,0 +1,80 @@
+public class Operators5 {
+    public static void main(String[] args) {
+        Operators5 sample = new Operators5();
+        //sample.additive();
+        //sample.remainder();
+        //sample.plusParami(1, 2);
+        //sample.increDecre(3);
+        //sample.comparison();
+        //sample.doBlindDate(70);
+        //sample.doBlindDate(80);
+    }
+
+    /**계산 예시 5와 10 사용 */
+    public void additive(){
+
+        int value1 = 5;
+        int value2 = 10;
+
+        int result = value1 + value2;
+        System.out.println("더하기"+result);
+        result = value2 - value1;
+        System.out.println("빼기"+result);
+    }
+
+    /** */
+    public void remainder(){
+        int value1 = 53;
+        int value2 = 10;
+
+        int result = value1 % value2;
+
+        System.out.println(result);
+    }
+
+    /**
+     * a에서 b만큼 증감하는 함수
+     * @param start 증감을 시작할 값
+     * @param plus start에서 증감할 값
+     */
+    public void plusParami(int start,int plus){
+        int result = start;
+        result += plus;
+
+       System.out.println("증감"+result);
+    }
+
+    /**
+     * 증감 감소하는 함수
+     * @param start 증감 시작하는 값
+     */
+    public void increDecre(int start){
+        int result = start;
+        System.out.println(result++); //증감안된 값 출력
+        System.out.println(result); // 증감된값 출력
+        System.out.println(++result); // 증감되어 출력
+    }
+
+    public void comparison(){
+        int value1 =1;
+        int value2 =2;
+        int value3 =3;
+
+        System.out.println(value1 == value2);
+        System.out.println(value1 != value3);
+    }
+
+
+    /**
+     * 점수에 대한 합격 불합격 판단 하는 함수
+     * @param point 해당점수
+     */
+    public void doBlindDate(int point){
+        boolean result = false;
+        result = (point>=80) ? true : false;
+        System.out.println(point + " => " + result);
+    }
+
+    
+
+}
