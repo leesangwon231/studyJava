bee711c207d13cb315264cec747da0604cb249fa
Sangwon Lee <werggf@naver.com>
2022-10-05 21:39:06 +0900

    카드 셔플 다시 생각해보기

--- bakjun/EightySeven.java
+++ bakjun/EightySeven.java
@@ -4,14 +4,11 @@ import java.util.Scanner;
 
 public class EightySeven {
     public void sumMyself(int mySelf){
-        int sum = 0;
-        for (int i = 0; i < mySelf; i++) {
-            sum += i;
-            if(sum>mySelf){
-                break;
-            }
-        }
-        System.out.println(sum);
+       if(mySelf%2==0){
+            System.out.println("짝수");
+       }else{
+        System.out.println("홀수");
+       }
     }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
--- bakjun/Gridy/Card.java
+++ bakjun/Gridy/Card.java
@@ -1,6 +1,9 @@
 package bakjun.Gridy;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class Card {
@@ -15,16 +18,51 @@ public class Card {
             cardS[i] = scanner.nextInt();
         }
 
-        Arrays.sort(cardS);
-
+       
+        
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        
         int memory = cardS[0]+cardS[1];
         
         for (int i = 2; i < cardS.length; i++) {
-            memory += (memory+cardS[i]);
-        }
+            
+            if(memory>cardS[i]){
+                a.add(memory);
+                memory = cardS[i];
+            }else{
+                memory += cardS[i];
+            }
+            
+        } // 10 10 20 20 30 
+        a.add(memory); //마지막 40 50
+        
+        memory = 0; // 20 30 20 -> 20 20 30 ->  20 30 -> 30 40 
+        
+        while(true){
+            Collections.sort(a);
+            memory = a.get(0);
+            ArrayList<Integer> b = new ArrayList<Integer>();
+            for (int i = 1; i < a.size(); i++) {
+                if(memory>a.get(i)){
+                    b.add(memory);
+                    memory = a.get(i);    
+                }else{
+                    memory += a.get(i);
+                }
+                
+            }
 
-        System.out.println(memory);
+            a.add(memory); 
+            for (int i = 0; i < a.size(); i++) {
+                b.add(a.get(i));
+            }
+            a.removeAll(a);
+            if(b.size()==2){
+                break;
+            }
+        }
        
+        System.out.println(a.get(0)+a.get(1));
         scanner.close();
     }
 
